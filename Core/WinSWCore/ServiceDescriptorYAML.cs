c6a3cdc089a5514cca509634005f25e789d711a0
Buddhika Chathuranga <vmbckscc456@gmail.com>
2020-02-21 15:51:53 +0530

    SingleTImeSpanElement method added

--- Core/WinSWCore/ServiceDescriptorYAML.cs
+++ Core/WinSWCore/ServiceDescriptorYAML.cs
@@ -122,6 +122,44 @@ namespace winsw
             return node == null ? defaultValue : int.Parse(node.ToString());
         }
 
+        private TimeSpan SingleTimeSpanElement(string tagName, TimeSpan defaultValue)
+        {
+            var node = SingleElement(tagName, true);
+
+            return node == null ? defaultValue : ParseTimeSpan(node); 
+        }
+
+        private TimeSpan ParseTimeSpan(string v)
+        {
+            v = v.Trim();
+            foreach (var s in Suffix)
+            {
+                if (v.EndsWith(s.Key))
+                {
+                    return TimeSpan.FromMilliseconds(int.Parse(v.Substring(0, v.Length - s.Key.Length).Trim()) * s.Value);
+                }
+            }
+
+            return TimeSpan.FromMilliseconds(int.Parse(v));
+        }
+
+        private static readonly Dictionary<string, long> Suffix = new Dictionary<string, long>
+        {
+            { "ms",     1 },
+            { "sec",    1000L },
+            { "secs",   1000L },
+            { "min",    1000L * 60L },
+            { "mins",   1000L * 60L },
+            { "hr",     1000L * 60L * 60L },
+            { "hrs",    1000L * 60L * 60L },
+            { "hour",   1000L * 60L * 60L },
+            { "hours",  1000L * 60L * 60L },
+            { "day",    1000L * 60L * 60L * 24L },
+            { "days",   1000L * 60L * 60L * 24L }
+        };
+
+
+
         public string Executable => SingleElement("executable");
        
         public bool HideWindow => SingleBoolElement("hidewindow", Defaults.HideWindow);
